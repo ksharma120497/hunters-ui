425412b30848978ec2b4d7e8cc96ca9cfbb5a729
Kapil Sharma <ksharma120497@gmail.com>
2023-06-02 03:29:20 +0530

    basic structure of the code

---
+++ src/components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeScreen from './home/HomeScreen';
+
+const MainContainer = () => {
+
+    return (
+    <div>
+        <HomeScreen />
+    </div>
+    )
+
+}
+
+export default MainContainer;
---
+++ src/components/home/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style'
+
+const HomeScreen = () =>{
+    return (
+        <div>
+            <p>
+                
+            </p>
+        </div>
+    )
+}
+
+export default HomeScreen;
